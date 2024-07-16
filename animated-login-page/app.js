500bececbcb653a22b28dff405fe23d7c8e8f22f
TubaNS <tubanursaka@gmail.com>
2024-07-16 19:44:48 +0300

    js

--- animated-login-page/app.js
+++ animated-login-page/app.js
@@ -1,16 +1,25 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const music = document.getElementById('bck-music');
-    const playMusic = () => {
-      music.play().catch(error => {
-        console.log('Otomatik oynatma başarısız oldu:', error);
-      });
-    };
-  
-    if (music) {
-      playMusic(); 
+
+    // Müziği otomatik olarak başlatma işlemi
+    function startMusic() {
+        if (music) {
+            music.play().then(() => {
+                console.log('Müzik başarıyla başladı.');
+            }).catch(error => {
+                console.log('Müzik otomatik başlatma başarısız:', error);
+            });
+        }
     }
-  });
 
+    // Sayfa yüklendiğinde veya belirli bir süre sonra müziği başlatma
+    startMusic();
 
+    // Kullanıcı etkileşimiyle müziği başlatma
+    document.addEventListener('click', function(event) {
+        const target = event.target;
+        if (target && target.classList.contains('login')) {
+            startMusic();
+        }
+    });
+});
